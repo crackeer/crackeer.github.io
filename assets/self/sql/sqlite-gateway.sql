7d6b539dbe01de77d86d0685881b79bb6ea8ab3f
liuhu016 <liuhu016@ke.com>
2023-04-29 16:53:38 +0800

    update

---
+++ assets/self/sql/sqlite-gateway.sql
@@ -0,0 +1,43 @@
+CREATE TABLE service(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name           TEXT    NOT NULL,
+   service        TEXT    NOT NULL,
+   host           TEXT     NOT NULL,
+   env TEXT NOT NULL,
+   timeout int,
+   sign TEXT NOT NULL,
+   sign_config TEXT NOT NULL,
+   response_config TEXT NOT NULL,
+   description TEXT NOT NULL,
+   create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   modify_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_service_env on service (service, env);
+
+
+CREATE TABLE service_api(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   service        TEXT    NOT NULL,
+   api TEXT NOT NULL,
+   path TEXT NOT NULL,
+   method TEXT NOT NULL,
+   content_type TEXT NOT NULL,
+   timeout int,
+   description TEXT NOT NULL,
+   create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   modify_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_service_api on service_api (service, api);
+
+CREATE TABLE router (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   path           TEXT    NOT NULL,
+   status INT NOT NULL,
+   request_type        TEXT    NOT NULL,
+   request_config TEXT NOT NULL,
+   response_type TEXT NOT NULL,
+   response_config TEXT NOT NULL,
+   description TEXT NOT NULL,
+   create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   modify_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
